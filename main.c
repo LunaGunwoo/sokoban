1cacb0a4f3a7ed37f74070fadf912b156bf81abf
Gunwoo Moon <moongunwoo7019@naver.com>
2026-05-05 17:14:07 +0900

    inital screen

--- main.c
+++ main.c
@@ -1,6 +1,90 @@
 #include <stdio.h>
+#include <termio.h>
 
-int main(int, char**) {
-  printf("Hello, from sokoban!\n");
+int getch(void);
+void show_inital_screen();
+void show_option_screen();
+void show_name_screen();
+
+int main(void) {
+  char maps[3][30][30] = {{
+                              "....#####..........",
+                              "....#...#..........",
+                              "....#$..#..........",
+                              "..###..$##.........",
+                              "..#..$.$.#.........",
+                              "###.#.##.#...######",
+                              "#...#.##.#####..OO#",
+                              "#.$..$..........OO#",
+                              "#####.###.#@##..OO#",
+                              "....#.....#########",
+                              "....#######........",
+                          },
+                          {
+                              "############..",
+                              "#OO..#.....###",
+                              "#OO..#.$..$..#",
+                              "#OO..#$####..#",
+                              "#OO....@.##..#",
+                              "#OO..#.#..$.##",
+                              "######.##$.$.#",
+                              "..#.$..$.$.$.#",
+                              "..#....#.....#",
+                              "..############",
+                          },
+                          {
+                              "........########.",
+                              "........#.....@#.",
+                              "........#.$#$.##.",
+                              "........#.$..$#..",
+                              "........##$.$.#..",
+                              "#########.$.#.###",
+                              "#OOOO..##.$..$..#",
+                              "##OOO....$..$...#",
+                              "#OOOO..##########",
+                              "########.........",
+                          }};
+  char option;
+  char name[3];
+
+  show_inital_screen();
+  show_option_screen();
+  scanf("%c", &option);
+  show_name_screen();
+  scanf("%c", name);
   return 0;
 }
+
+int getch(void) {
+  int ch;
+
+  struct termios buf;
+  struct termios save;
+
+  tcgetattr(0, &save);
+  buf = save;
+
+  buf.c_lflag &= ~(ICANON | ECHO);
+  buf.c_cc[VMIN] = 1;
+  buf.c_cc[VTIME] = 0;
+
+  tcsetattr(0, TCSAFLUSH, &buf);
+
+  ch = getchar();
+  tcsetattr(0, TCSAFLUSH, &save);
+
+  return ch;
+}
+
+void show_inital_screen() {
+  printf("=======================================\n");
+  printf("       S   O   K   O   B   A   N       \n");
+  printf("=======================================\n");
+  printf("\n");
+  printf("n : New Game\n");
+  printf("1~3 : Level Number\n");
+  printf("\n\n");
+}
+
+void show_option_screen() { printf("Input option : "); }
+void show_name_screen() { printf("Input your name : "); }
